be8332cbfc821379a36b3a8ddb883453e1b0e749
Vison Coded Studio <visioncodedstudio@gmail.com>
2026-03-25 09:46:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
 
+if (toggle && navLinks) {
+  toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
